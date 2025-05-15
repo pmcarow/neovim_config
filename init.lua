a1bfe409c6f8463f922b493fd75bbb69c94540d5
BLUEORIGIN\pcarow <pcarow@blueorigin.com>
2025-05-15 06:02:43 -0600

    Added yank keymap

--- init.lua
+++ init.lua
@@ -13,3 +13,16 @@ vim.opt.rtp:prepend(lazypath)
 
 require("vim-options")
 require("lazy").setup("plugins")
+
+vim.g.clipboard = {
+    name = "win32yank",
+    copy = {
+        ["+"] = "win32yank.exe -i --crlf",
+        ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+        ["+"] = "win32yank.exe -o --lf",
+        ["*"] = "win32yank.exe -o --lf",
+    },
+    cache_enabled = false,
+}
