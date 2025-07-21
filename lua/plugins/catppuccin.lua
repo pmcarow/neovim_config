3c899cd5c9095b8bc48126f0afaae8bd1f27a5ba
pmcarow <pmcarow@gmail.com>
2025-07-21 11:33:43 -0600

    Update catppuccin.lua

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -5,6 +5,16 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
+      require("catppuccin").setup({
+        transparent_background = true,
+        custom_highlights = function(colors)
+          return {
+            Comment = { fg = "#ffffe0" }, -- Very light yellow for comments
+            LineNr = { fg = "#ffffff" },  -- White for line numbers
+            CursorLineNr = { fg = "#ffffff" }, -- White for current line number
+          }
+        end
+      })
       vim.cmd.colorscheme "catppuccin-mocha"
     end
   }
