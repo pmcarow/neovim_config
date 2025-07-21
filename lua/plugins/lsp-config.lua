716a026311784d8ca9469b4cba27b55617711903
pmcarow <pmcarow@gmail.com>
2025-07-21 11:30:35 -0600

    Update lsp-config.lua

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = {"lua_ls", "ts_ls", "pyright"}
+        ensure_installed = {"lua_ls", "ts_ls", "pyright","clangd"}
       })
     end
   },
@@ -20,6 +20,7 @@ return {
       lspconfig.lua_ls.setup({})
       lspconfig.ts_ls.setup({})
       lspconfig.pyright.setup({})
+      lspconfig.clangd.setup({})
       vim.keymap.set('n','K',vim.lsp.buf.hover, {})
     end
   }
